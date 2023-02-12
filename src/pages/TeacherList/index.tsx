3ab7c439f2999b07dfb70db1628dd9b74b170ab1
Washington Charles <toncharles@live.com>
2023-02-12 20:17:02 -0300

    component teacherItem

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,6 +1,5 @@
 import { PageHeader } from '../../components/Pageheader';
-
-import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+import { TeacherItem } from '../../components/TeacherItem';
 
 import './styles.css';
 
@@ -27,33 +26,9 @@ export function TeacherList() {
       </PageHeader>
 
       <main>
-        <article className="teacher-item">
-          <header>
-            <img src="https://github.com/washingtoncharles.png" alt="Avatar" />
-            <div>
-              <strong>Washington Charles</strong>
-              <span>Química</span>
-            </div>
-          </header>
-
-          <p>
-            Entusiasta das melhores tecnologias de química avançada.
-            <br /><br />
-            Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
-          </p>
-
-          <footer>
-            <p>
-              Preço/hora
-              <strong>R$ 60,00</strong>
-            </p>
-            <button type='button'>
-              <img src={whatsappIcon} alt="Whatsapp" />
-              Entrar em contato
-            </button>
-          </footer>
-
-        </article>
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
       </main>
     </div>
   )
